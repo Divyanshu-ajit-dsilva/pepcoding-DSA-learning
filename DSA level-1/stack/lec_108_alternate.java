9bf0fe7a4a2578f9ef8918077ed439afc94253aa
Divyanshu <divyanshudsilva@gmail.com>
2024-04-25 11:13:31 +0530

    Uploaded the file for the next greater element on the right alternate solution

---
+++ DSA level-1/stack/lec_108_alternate.java
@@ -0,0 +1,53 @@
+// Program to find the largest integer to the right of an element in an array
+import java.io.*;
+import java.util.*;
+
+public class lec_108_alternate{
+    public static void display(int[] a){
+        StringBuilder sb = new StringBuilder();
+        
+        for(int val: a){
+            sb.append(val+" ");
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = Integer.parseInt(br.readLine());
+
+        }
+
+        int[] nge = solve(a);
+        display(nge);
+    }
+
+    public static int[] solve(int[] arr){
+        int[] nge = new int[arr.length];
+
+        Stack<Integer> st = new Stack<>();
+
+        st.push(0);
+        for(int i = 1; i < arr.length; i++){
+            while (st.size() > 0 && arr[i] > arr[st.peek()]) {
+                int pos = st.peek();
+                nge[pos] = arr[i];
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        while (st.size() > 0) {
+            int pos = st.peek();
+                nge[pos] = -1;
+                st.pop();
+        }
+
+        return nge;
+    }
+}
